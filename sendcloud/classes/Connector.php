2f6bdd2b7387c7f894eadc43be6aa0ddab55ffd0
Erick Wilder <erick.wilder@sendcloud.com>
2019-11-12 15:59:52 +0100

    fix(connector): typo on `@link` docs

--- sendcloud/classes/Connector.php
+++ sendcloud/classes/Connector.php
@@ -9,7 +9,7 @@
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  @category  Shipping
  *  @package
- *  @link      https:/.eu
+ *  @link      https://sendcloud.com
  */
 
 /**
