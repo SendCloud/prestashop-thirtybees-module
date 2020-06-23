23001a0b8e759f610f05210ffdbce60f52d6fa2c
Tim Drijvers <tim@sendcloud.com>
2020-06-23 11:07:28 +0200

    feat: request access to currencies resources

--- sendcloud/classes/Connector.php
+++ sendcloud/classes/Connector.php
@@ -99,6 +99,7 @@ class SendcloudConnector
     private $apiResources = array(
         'addresses',
         'carriers',
+        'currencies',
         'configurations',
         'countries',
         'customers',
