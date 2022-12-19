9f6858b76711ec4b381adaa0c096c657cf48a716
piotrgolonko <piopiotrek008@gmail.com>
2022-12-19 11:24:03 +0100

    feat(prestashop): combinations resources are necessary to correctly establish weight of products

--- sendcloud/sendcloud.php
+++ sendcloud/sendcloud.php
@@ -56,7 +56,7 @@ class Sendcloud extends CarrierModule
         $this->boostrap = true;
         $this->name = 'sendcloud';
         $this->tab = 'shipping_logistics';
-        $this->version = '1.4.2';
+        $this->version = '1.5.0';
         $this->author = 'Sendcloud';
         $this->author_uri = 'https://sendcloud.com';
         $this->need_instance = false;
